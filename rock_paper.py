55c0d57ac2e2a4d0c564dc639d5fc50cf0d8f4a7
Renee127 <64703105+renee127@users.noreply.github.com>
2020-08-06 01:14:59 -0400

    simple version of rock paper scissors game

---
+++ rock_paper.py
@@ -0,0 +1,32 @@
+"""Rock, paper, scissors game with computer oponent.
+"""
+from random import randint
+
+# get random choice to represent computer's play
+choices = ['Rock', 'Paper', 'Scissors']
+computer_choice = choices[randint(0,2)]
+print(computer_choice)
+
+# get user choice
+player_choice = input('Rock, Paper, or Scissors?  ')
+
+# compare choices and determine win lose tie
+if player_choice == computer_choice:
+    print('Tie')
+elif player_choice == 'Rock':
+    if computer_choice == 'Paper':
+        print('Paper covers Rock: you lose.')
+    elif computer_choice == 'Scissors':
+        print('Rock smashes Scissors: you win.')
+elif player_choice == 'Scissors':
+    if computer_choice == 'Paper':
+        print('Scissors cut Paper: you win.')
+    elif computer_choice == 'Rock':
+        print('Rock smashes Scissors: you lose.')
+elif player_choice == 'Paper':
+    if computer_choice == 'Scissors':
+        print('Scissors cut Paper: you lose.')
+    elif computer_choice == 'Rock':
+        print('Paper covers Rock: you win.')
+else:
+    print('Please choose Rock, Paper, or Scissors')
