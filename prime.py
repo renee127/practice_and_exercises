7933c7dd41566caae6f3d15378c06f846ec96d77
Renee127 <64703105+renee127@users.noreply.github.com>
2020-10-04 19:41:29 -0400

    ran through pylint

--- prime.py
+++ prime.py
@@ -1,18 +1,16 @@
-# Takes a number as input and 
-# returns the prime numbers 
-
-number = int(input('Please enter a number: '))
-
-factors = list()
-factor = 2
-# Create a loop to factorize number
-while factor <= number:
-    if number % factor == 0:
-        number = int(number / factor)
-        print(number)
-        factors.append(factor)
-    else:
-        factor += 1
-print(factors)
-
-
+"""Prime factor finder."""
+def main():
+    """Takes a number and finds prime factors."""
+    number = int(input('Please enter a number: '))
+    factors = list()
+    factor = 2
+    # Create a loop to factorize number
+    while factor <= number:
+        if number % factor == 0:
+            number = int(number / factor)
+            print(number)
+            factors.append(factor)
+        else:
+            factor += 1
+    print(factors)
+main()
